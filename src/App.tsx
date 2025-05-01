e2ee65291e05c5b6ac11371f6d69cce8c3a92609
LRLayan <rameshlayan4@gmail.com>
2025-05-02 03:49:56 +0530

    define routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css'
+import {createBrowserRouter, RouterProvider} from "react-router";
+import SignInAndSignUp from "./view/SignInAndSignUp.tsx";
 
 function App() {
 
+  const routes = createBrowserRouter([
+    {path: "/", element: <SignInAndSignUp/>}
+  ]);
+
   return (
     <>
-
+        <RouterProvider router={routes}/>
     </>
   )
 }
