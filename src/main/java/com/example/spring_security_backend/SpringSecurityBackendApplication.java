468ee0e0a6c3954b9cf3c923c1d0d618091f0ae8
LRLayan <rameshlayan4@gmail.com>
2025-05-02 19:50:14 +0530

    modelMapper()

--- src/main/java/com/example/spring_security_backend/SpringSecurityBackendApplication.java
+++ src/main/java/com/example/spring_security_backend/SpringSecurityBackendApplication.java
@@ -1,7 +1,9 @@
 package com.example.spring_security_backend;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringSecurityBackendApplication {
@@ -10,4 +12,8 @@ public class SpringSecurityBackendApplication {
         SpringApplication.run(SpringSecurityBackendApplication.class, args);
     }
 
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
