bd072be8aa767851d6b3affaa50974d7222fb4d6
LRLayan <rameshlayan4@gmail.com>
2025-05-02 19:48:49 +0530

    JWTAuthResponse

---
+++ src/main/java/com/example/spring_security_backend/security/jwt/JWTAuthResponse.java
@@ -0,0 +1,14 @@
+package com.example.spring_security_backend.security.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class JWTAuthResponse {
+    private String token;
+}
