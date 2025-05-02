bcdb21c73508aa1717ebf8a0e1535b26bc37c3ba
LRLayan <rameshlayan4@gmail.com>
2025-05-02 13:46:44 +0530

    set token in to request

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,21 @@
 import axios from "axios";
+import Cookies from 'js-cookie';
 
 export const api = axios.create({
     baseURL: "http://localhost:8080/api/v1/"
 });
+
+api.interceptors.request.use(
+    (config: any) => {
+        if (!config.url?.includes("/auth")) {
+            const token = Cookies.get("jwtToken");
+            if (token) {
+                config.headers["Authorization"] = `Bearer ${token}`;
+            }
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
