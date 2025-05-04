3647a4fce9082d9d78980c714043b9f2a12fec5d
LRLayan <rameshlayan4@gmail.com>
2025-05-05 00:07:04 +0530

    add RootLayout path in Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import {createBrowserRouter, RouterProvider} from "react-router";
 import SignInAndSignUp from "./view/SignInAndSignUp.tsx";
+import RootLayout from "./component/RootLayout.tsx";
 
 function App() {
 
   const routes = createBrowserRouter([
-    {path: "/", element: <SignInAndSignUp/>}
+    {path: "/", element: <SignInAndSignUp/>},
+    {path: "/home", element: <RootLayout/>}
   ]);
 
   return (
